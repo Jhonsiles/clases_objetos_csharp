f3213ea4f8ea681fb4b223c384109f50955b83e3
Jhosiles <jheremyjhonsilesj@gmail.com>
2025-08-22 16:33:35 -0400

    clases y objetos

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+//Jheremy Siles - Liceo Oscar Bonilla A-26
+public class Estudiante
+{
+    public string? Nombre { get; set; }
+    public string? Apellido { get; set; }
+
+    public void Informacion()
+    {
+        Console.WriteLine($"Bienvenido: {Nombre} {Apellido}");
+    }
+}
+
+//Clase principal del programa
+public class Program
+{
+    public static void Main()
+    {
+        Estudiante estudiante1 = new Estudiante();
+        estudiante1.Nombre = "Jhon";
+        estudiante1.Apellido = "Siles";
+        estudiante1.Informacion();
+        Estudiante estudiante2 = new Estudiante();
+        estudiante2.Nombre = "Jhon";
+        estudiante2.Apellido = "Siles";
+        estudiante2.Informacion();
+    }
+}
